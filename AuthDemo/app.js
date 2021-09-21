67caf40c3416474a9a3ae2cb8e7bd6f8388b1f02
Mukul Saini <thisismemukul@gmail.com>
2021-09-21 22:43:10 +0530

    requirements completed

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -1,13 +1,32 @@
-var express = require("express");
-app = express();
+const express = require("express"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    bodyParser = require("body-parser"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose"),
+    User = require("./models/user");
+const app = express();
 
-app.set("view-engine", "ejs");
+mongoose.connect("mongodb://localhost/auth");
 
+app.set("view engine", "ejs");
+
+app.use(require("express-session")({
+    secret: "You know thisisme Mukul",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.get("/", function(req, res) {
     res.render("home");
 });
 
-app("*", function(req, res) {
+app.get("/secret", function(req, res) {
+    res.render("secret");
+});
+
+app.get("*", function(req, res) {
     res.send("404 Page not found");
 });
 
