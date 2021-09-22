668df86dd9da073ae6fc1273529e43e2ad5d4ced
Mukul Saini <thisismemukul@gmail.com>
2021-09-22 17:51:59 +0530

    logout middleware

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -33,7 +33,7 @@ app.get("/", function(req, res) {
     res.render("home");
 });
 
-app.get("/secret", function(req, res) {
+app.get("/secret", isLoggedIn, function(req, res) {
     res.render("secret");
 });
 
@@ -69,6 +69,18 @@ app.post("/login", passport.authenticate("local", {
 
 });
 
+//LOGOUT
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
 app.get("*", function(req, res) {
     res.send("404 Page not found");
 });
